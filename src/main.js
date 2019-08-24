4a7a947bdc464b680fdda90ce62fbdf929974ad5
lixinyuyu <591583917@qq.com>
2019-08-24 22:20:18 +0800

    项目初始化 && rem配置完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import store from './store'
-
+import vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 Vue.config.productionTip = false
-
+Vue.use(vant)
 new Vue({
   router,
   store,
---
+++ src/views/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    home
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <div class="box">111</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scoped>
+.box {
+  width: 750px;
+  height: 100px;
+  background-color: red;
+}
+</style>

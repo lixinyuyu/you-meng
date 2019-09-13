0135af9c86875d9fbb2c4f42ed871af1af87cee3
lixinyuyu <591583917@qq.com>
2019-09-14 02:46:39 +0800

    头像预览成功

--- src/views/home/components/avator-dialog.vue
+++ src/views/home/components/avator-dialog.vue
@@ -9,7 +9,7 @@
     >
       <van-cell-group>
         <van-cell title="从相册选择" @click="$refs['file'].click()" />
-        <input type="file"  id="fils" style="display: none" ref="file">
+        <input type="file"  id="fils" style="display: none" ref="file" @change="handleImgChange">
         <van-cell title="单元格"  />
         <van-cell title="取消"  @click="$emit('input', false)"/>
       </van-cell-group>
@@ -18,6 +18,9 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import { ImagePreview } from 'vant'
+Vue.use(ImagePreview)
 export default {
   props: {
     value: {
@@ -27,6 +30,24 @@ export default {
   },
   data () {
     return {
+    }
+  },
+  computed: {
+    file () {
+      return this.$refs['file']
+    }
+  },
+  methods: {
+    handleImgChange () {
+      const reader = new FileReader()
+      reader.readAsDataURL(this.file.files[0])
+      reader.onload = () => {
+        ImagePreview({
+          images: [
+            reader.result
+          ]
+        })
+      }
     }
   }
 }
